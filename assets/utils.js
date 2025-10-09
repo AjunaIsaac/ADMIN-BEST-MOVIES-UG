c46cb329464ea6d5c6c0ca6727012737738c3afc
ajunaiaac <ajunaisaaconline@gmail.com>
2025-10-10 02:23:03 +0300

    g

--- assets/utils.js
+++ assets/utils.js
@@ -93,7 +93,7 @@ async function checkForDuplicates(payload, excludeDocId = null) {
 async function sendNotification(title, body, imageUrl = '', url = '', msgElement) {
     const payload = { data: { title, body, imageUrl, url } };
     try {
-        const functionUrl = 'https://bestmovies.ajunaisaac-ug.workers.dev';
+        const functionUrl = 'https://best-movies-worker.ajunaisaac-ug.workers.dev';
         const response = await fetch(functionUrl, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
